b18a374d465ba0e306e9d0d206fb8fd8661ab0a2
Ruben Meijers <rubenmeijers18@gmail.com>
2019-05-18 17:29:36 +0200

    Added InvoiceService

---
+++ Airport/InvoiceService/Events/CustomerRegistered.cs
@@ -0,0 +1,24 @@
+﻿using Airport.Infrastructure.Messaging;
+using System;
+
+namespace Airport.InvoiceService.Events
+{
+    public class CustomerRegistered : Event
+    {
+        public readonly string CustomerId;
+        public readonly string Name;
+        public readonly string Address;
+        public readonly string PostalCode;
+        public readonly string City;
+
+
+        public CustomerRegistered(Guid messageId, string customerId, string name, string address, string postalCode, string city) : base(messageId)
+        {
+            CustomerId = customerId;
+            Name = name;
+            Address = address;
+            PostalCode = postalCode;
+            City = city;
+        }
+    }
+}
