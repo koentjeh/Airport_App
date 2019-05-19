daaf1b9b1ed777d0799c6b9e86a5abc466cd92a6
Jari Verhaard <jjac.verhaard@student.avans.nl>
2019-05-19 15:48:15 +0200

    Rental API

---
+++ Airport/RentalManagementAPI/Events/RentalRegistered.cs
@@ -0,0 +1,27 @@
+﻿using Airport.Infrastructure.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Airport.RentalManagementAPI.Events
+{
+    public class RentalRegistered : Event
+    {
+        public readonly string RentalId;
+        public readonly string RenterId;
+        public readonly string Location;
+        public readonly string Price;
+        public readonly string StartDate;
+        public readonly string EndDate;
+
+        public RentalRegistered(Guid messageId, string rentalId, string renterId, string location, string price, string startDate, string endDate) : base(messageId)
+        {
+            RentalId = rentalId;
+            RenterId = renterId;
+            Location = location;
+            Price = price;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+    }
+}
---
+++ Airport/RentalManagementAPI/Migrations/RentalManagementDBContextModelSnapshot.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Airport.RentalManagementAPI.DataAccess;
+
+namespace Airport.RentalManagementAPI.Migrations
+{
+    [DbContext(typeof(RentalManagementDBContext))]
+    partial class RentalManagementDBContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0");
+
+            modelBuilder.Entity("Airport.RentalManagementAPI.Model.Rental", b =>
+            {
+                b.Property<string>("RentalId")
+                    .ValueGeneratedOnAdd();
+
+                b.Property<string>("RenterId");
+
+                b.Property<string>("Location");
+
+                b.Property<string>("Price");
+
+                b.Property<string>("StartDate");
+
+                b.Property<string>("EndDate");
+
+                b.HasKey("RentalId");
+
+                b.ToTable("Rental");
+            });
+        }
+    }
+}
