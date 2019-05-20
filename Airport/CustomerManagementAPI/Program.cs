5b8fc22943b388ae1dd58a7579c1f77555ed347f
Ruben Meijers <rubenmeijers18@gmail.com>
2019-05-20 19:52:56 +0200

    NameSpacing CustomerManagementAPI

--- Airport/CustomerManagementAPI/Program.cs
+++ Airport/CustomerManagementAPI/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace CustomerManagementAPI
+namespace Airport.CustomerManagementAPI
 {
     public class Program
     {
